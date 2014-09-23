5e5d74d7a87bacc0f2d17fdb0956c92720506784
Anthony Cerritelli <Anthony.Cerritelli@espn.com>
2014-09-23 13:38:31 -0400

    testing different syntax of search

--- definitions/mongodb.rb
+++ definitions/mongodb.rb
@@ -204,9 +204,10 @@ define :mongodb_instance,
   # replicaset
   if new_resource.isReplicaset && new_resource.auto_configure_replicaset
     rs_nodes = search(:node, "mongodb_clusterName:#{new_resource.replicaset['mongodb']['clusterName']}",
-       "mongodb_isReplicaset:true")
-       # mongodb_shardName:#{new_resource.replicaset['mongodb']['shardName']} AND \
-       # chef_environment:#{new_resource.replicaset.chef_environment}"
+       "mongodb_isReplicaset:true",
+       "mongodb_shardName:#{new_resource.replicaset['mongodb']['shardName']}",
+       "chef_environment:#{new_resource.replicaset.chef_environment}"
+    )
 
     ruby_block 'config_replicaset' do
       block do
